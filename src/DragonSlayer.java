22e02b0567bb7522d3dd80213ace1c2569d6b9b0
student <sample@bths.edu>
2023-01-18 12:32:32 -0500

    commit

--- src/DragonSlayer.java
+++ src/DragonSlayer.java
@@ -11,19 +11,34 @@ public class DragonSlayer {
         printMainMenu();
         System.out.print("Enter choice: ");
         String choice = scan.nextLine();
-        if (choice.equals("V") || choice.equals("v")){
-            System.out.println("Your top score is: " + topScore + "!");
+        String returnToMain = "";
+        while (choice.equals("V") || choice.equals("v") && !(returnToMain.equals("n") || returnToMain.equals("N"))){
+            if (choice.equals("V") || choice.equals("v")){
+                System.out.println("Your top score is: " + topScore + "!");
+            }
+            System.out.print("Would you like to return to the main menu? (y/n) ");
+            returnToMain = scan.nextLine();
+            if (returnToMain.equals("Y") || returnToMain.equals("y")){
+                printMainMenu();
+                System.out.print("Enter choice: ");
+                choice = scan.nextLine();
+            }
+
         }
-        if (choice.equals("Q") || choice.equals("q")){
+        if (choice.equals("Q") || choice.equals("q") ||returnToMain.equals("n") || returnToMain.equals("N") ){
             System.out.println("Thank you for playing!");
         }
         if (choice.equals("S") || choice.equals("s")){
-            System.out.print("What is your name? Enter here: ");
+            System.out.print("What is your name? ");
             String name = scan.nextLine();
             p = new Player(name, 100, 0);
-            System.out.println("Alright, " + name + ", your goal is to clear 5 rooms of the dragon's lair.");
+            System.out.println(name + ", your goal is to clear 5 rooms of the dragon's lair. Good luck!");
             enterRoom("the Hatchery");
+            printMenu();
+            System.out.print("What's your next move? ");
+            String choice2 = scan.nextLine();
         }
+
     }
 
     //private helper methods
@@ -45,6 +60,17 @@ public class DragonSlayer {
         System.out.println("            (V)iew top score              ");
         System.out.println("                (Q)uit                    ");
         System.out.println("           -----------------              ");
+    }
+
+    private void printMenu(){
+        System.out.println("------------ Menu ------------");
+        System.out.println("(A)ttack dragon");
+        System.out.println("(I)nspect dragon's level");
+        System.out.println("(C)heck health");
+        System.out.println("(V)iew weapon stats");
+        System.out.println("(S)earch room");
+        System.out.println("(E)nter next room");
+        System.out.println("------------------------------");
     }
 
 
